9ee6d202be159aa51bcc343a8d022e4aecd39415
jhonnferrerosa <jhonn.trucke@gmail.com>
2023-07-13 00:54:58 +0200

    primer commit

---
+++ b.cpp
@@ -0,0 +1,17 @@
+
+//este es una pruaba de C++
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main (){
+	printf ("\n hola mundo desde C++, lo subo a git");
+	
+	
+	printf ("\n");
+	return 0;
+}
+
